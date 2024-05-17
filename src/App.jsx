24b7420df87e5e4d01db99771bc62e2ee2f6ee62
Duong Minh Nhat <nhatdmse173465@fpt.edu.vn>
2024-05-17 16:30:32 +0700

    setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import LayoutMain from '@/components/layouts';
+
+function App() {
+  return <LayoutMain />;
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Lấy đường dẫn tuyệt đối đến tệp hiện tại
+const __filename = fileURLToPath(import.meta.url);
+// Lấy đường dẫn đến thư mục hiện tại
+const __dirname = path.dirname(__filename);
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 2003,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+});
