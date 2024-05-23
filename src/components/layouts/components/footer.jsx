6fa9d6bceaa4b4f2b1cc6e851187b1780fa6969d
luan0811 <luanntmse173311@fpt.edu.vn>
2024-05-23 08:43:07 +0700

    pull main + update aboutUs, footer

--- src/components/layouts/components/footer.jsx
+++ src/components/layouts/components/footer.jsx
@@ -1,47 +1,79 @@
-import { Col, Row } from 'antd';
+import { Col, Row } from "antd";
+import { Link } from "react-router-dom";
+import {
+  FacebookOutlined,
+  InstagramOutlined,
+  TwitterOutlined,
+  YoutubeOutlined,
+} from "@ant-design/icons";
 
-const footerStyle = { color: 'white', padding: '20px' };
-const columnStyle = { padding: '8px 0', color: 'white' };
+const footerStyle = { color: "white", padding: "20px" };
+const columnStyle = { padding: "8px 0", color: "white" };
 
 export default function FooterLayout() {
   return (
     <div style={footerStyle}>
       <Row
         gutter={16}
-        style={{ display: 'flex', justifyContent: 'space-around' }}
+        style={{ display: "flex", justifyContent: "space-around" }}
       >
-        <Col className='gutter-row' span={7}>
+        <Col className="gutter-row" span={7}>
           <div style={columnStyle}>
             <h3>GIỚI THIỆU</h3>
             <p>
-              Đặt sân 247 cung cấp các tiện ích thông minh giúp cho bạn tìm sân
-              cầu lông một cách hiệu quả nhất.
+              <strong>Racket Rise</strong> cung cấp các tiện ích thông minh giúp
+              cho bạn tìm sân cầu lông một cách hiệu quả nhất. Với hệ thống đặt
+              sân trực tuyến 24/7 bạn có thể dễ dàng tìm kiếm và đặt sân cầu
+              lông mọi lúc mọi nơi.
+              <br></br>
+              <Link
+                style={{ color: "white", textDecoration: "underline" }}
+                to="/aboutUs"
+              >
+                Về chúng tôi
+              </Link>
             </p>
           </div>
         </Col>
-        <Col className='gutter-row' span={4}>
+        <Col className="gutter-row" span={4}>
           <div style={columnStyle}>
-            <h3>CHÍNH SÁCH</h3>
-            <p>Chính sách bảo mật</p>
-            <p>Chính sách huỷ (đổi trả)</p>
-            <p>Chính sách kiểm hàng</p>
-            <p>Chính sách thanh toán</p>
+            <h3>THEO DÕI CHÚNG TÔI</h3>
+            <p>
+              <a href="https://facebook.com">
+                Facebook <FacebookOutlined />
+              </a>
+            </p>
+            <p>
+              <a href="https://instagram.com/">
+                Instagram <InstagramOutlined />
+              </a>
+            </p>
+            <p>
+              <a href="https://x.com">
+                Twitter <TwitterOutlined />
+              </a>
+            </p>
+            <p>
+              <a href="https://youtube.com/">
+                Youtube <YoutubeOutlined />
+              </a>
+            </p>
           </div>
         </Col>
-        <Col className='gutter-row' span={7}>
+        <Col className="gutter-row" span={7}>
           <div style={columnStyle}>
-            <h3>THÔNG TIN</h3>
-            <p>Công ty Cổ phần Booking 247 MST 0110175404</p>
-            <p>Email: contact@datsan247.com</p>
+            <h3>THÔNG TIN VÀ LIÊN LẠC</h3>
+            <p>Tên công ty: Racket Rise</p>
+            <p>Email: luanntmse173311@fpt.edu.vn</p>
             <p>
-              Địa chỉ: Số 3 ngõ 612/34/15 Đường La Thành, Phường Giảng Võ, Quận
-              Ba Đình, Thành phố Hà Nội, Việt Nam
+              Địa chỉ: Lô E2a-7, Đường D1, Đ. D1, Long Thạnh Mỹ, Thành Phố Thủ
+              Đức, Thành phố Hồ Chí Minh, Việt Nam
             </p>
             <p>Điện thoại: 0247.303.0247</p>
-            <p>
+            {/* <p>
               Giấy phép ĐKKD số 0110175404 do Sở Kế hoạch và Đầu tư thành phố Hà
               Nội cấp ngày 08/11/2022
-            </p>
+            </p> */}
           </div>
         </Col>
       </Row>
