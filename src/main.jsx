5eee3b8e0072b7aae69a6fe100aa48cbf84af37e
Duong Minh Nhat <nhatdmse173465@fpt.edu.vn>
2024-05-27 17:42:28 +0700

    UpdateCart-Redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,42 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { ConfigProvider } from "antd";
-import App from "@/App.jsx";
-import "@/styles/main.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
+import App from '@/App.jsx';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
+import '@/styles/main.css';
 
 const theme = {
   token: {
-    colorPrimary: "#FFCD1C",
-    colorBgLayout: "linear-gradient(to left, #bcbcbc, #444444)",
-    colorTextPlaceholder: "silver.200",
-    colorBgInput: "white",
-    addonBg: "white",
+    colorPrimary: '#FFCD1C',
+    colorBgLayout: 'linear-gradient(to left, #bcbcbc, #444444)',
+    colorTextPlaceholder: 'silver.200',
+    colorBgInput: 'white',
+    addonBg: 'white',
   },
   components: {
     Button: {
       algorithm: true,
-      colorText: "black",
+      colorText: 'black',
       variants: {
         primary: {
-          backgroundColor: "#FFCD1C",
-          color: "white", // text color when the button has the primary color
+          backgroundColor: '#FFCD1C',
+          color: 'white', // text color when the button has the primary color
         },
       },
     },
   },
 };
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ConfigProvider theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ConfigProvider>
+    <Provider store={store}>
+      <ConfigProvider theme={theme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ConfigProvider>
+    </Provider>
   </React.StrictMode>
 );
