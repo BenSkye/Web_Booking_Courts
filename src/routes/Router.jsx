50531f58af2867e0eaee8e24842df78f1fae35c0
Duong Minh Nhat <nhatdmse173465@fpt.edu.vn>
2024-05-29 22:01:47 +0700

    UpdateAll

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,53 +1,55 @@
-import { Routes, Route } from "react-router-dom";
-import LayoutMain from "@/components/layouts";
-import Home from "@/pages/home";
-import AboutUs from "@/components/dashboard/aboutUs";
-import Partner from "@/components/dashboard/partner";
-import Tournament from "@/pages/tournament";
-import Login from "@/pages/login/login";
-import SignUp from "@/pages/login/signup";
-import Detail from "@/pages/detail/detail";
-import BookingDetail from "@/components/booking/bookingDetails";
-import ProfileAccount from "@/pages/profileAccount";
-import AccountSettingsForm from "@/pages/accountInformation";
-import RegisterPackageCourt from "@/components/registerPackageCourt";
-import TournamentCreate from "@/pages/tournamentCreate";
-import RegistTournamentForm from "@/pages/tournamentCreate/components/RegistTournamentForm";
-import CourtManage from "@/pages/courtManage";
-import TournamentDetail from "../pages/TournamentDetail";
+import { Routes, Route } from 'react-router-dom';
+import LayoutMain from '@/components/layouts';
+import Home from '@/pages/home';
+import AboutUs from '@/components/dashboard/aboutUs';
+import Partner from '@/components/dashboard/partner';
+import Tournament from '@/pages/tournament';
+import Login from '@/pages/login/login';
+import SignUp from '@/pages/login/signup';
+import Detail from '@/pages/detail/detail';
+import BookingDetail from '@/components/booking/bookingDetails';
+import ProfileAccount from '@/pages/profileAccount';
+import AccountSettingsForm from '@/pages/accountInformation';
+import RegisterPackageCourt from '@/components/registerPackageCourt';
+import TournamentCreate from '@/pages/tournamentCreate';
+import RegistTournamentForm from '@/pages/tournamentCreate/components/RegistTournamentForm';
+import CourtManage from '@/pages/courtManage';
+import TournamentDetail from '../pages/TournamentDetail';
 import UpdatePassword from '../pages/updatePassword/index';
+import PaymentBookingFixed from '../components/booking/bookingDetails/bookingFixedByMonth/components/payment/paymentBookingFixed';
 function Routing() {
   return (
     <Routes>
-      <Route path="/" element={<LayoutMain />}>
+      <Route path='/' element={<LayoutMain />}>
         <Route index element={<Home />} />
-        <Route path="/aboutUs" element={<AboutUs />} />
-        <Route path="/partner" element={<Partner />} />
-        <Route path="/detail/:id" element={<Detail />} />
-        <Route path="/tournament" element={<Tournament />} />
-        <Route path="/tournament/create" element={<TournamentCreate />} />
+        <Route path='/aboutUs' element={<AboutUs />} />
+        <Route path='/partner' element={<Partner />} />
+        <Route path='/detail/:id' element={<Detail />} />
+        <Route path='/tournament' element={<Tournament />} />
+        <Route path='/tournament/create' element={<TournamentCreate />} />
         <Route
-          path="/tournament/create/:centerID"
+          path='/tournament/create/:centerID'
           element={<RegistTournamentForm />}
         />
         {/* <Route
           path="/tournament/detail/:tournamentID"
           element={<TournamentDetail />}
         /> */}
-        <Route path="/bookingdetail/:id" element={<BookingDetail />} />
-        <Route path="/user" element={<ProfileAccount />} />
-        <Route path="/user/my-account/:id" element={<ProfileAccount/>} />
-        <Route path="/user/update-password/:id" element={<UpdatePassword/>} />
-        <Route path="/user/bill" element={<ProfileAccount/>} />
+        <Route path='/bookingdetail/:id' element={<BookingDetail />} />
+        <Route path='/user' element={<ProfileAccount />} />
+        <Route path='/user/my-account/:id' element={<ProfileAccount />} />
+        <Route path='/user/update-password/:id' element={<UpdatePassword />} />
+        <Route path='/user/bill' element={<ProfileAccount />} />
+        <Route path='/paymentBookingFixed' element={<PaymentBookingFixed />} />
         <Route
-          path="/registerPackageCourt"
+          path='/registerPackageCourt'
           element={<RegisterPackageCourt />}
         />
-        <Route path="/courtManage" element={<CourtManage />} />
+        <Route path='/courtManage' element={<CourtManage />} />
       </Route>
 
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<SignUp />} />
     </Routes>
   );
 }
---
+++ src/utils/getAllCenterMap/index.jsx
@@ -0,0 +1,121 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import ReactMapGL, { Marker, NavigationControl } from 'react-map-gl';
+import mapboxgl from 'mapbox-gl';
+import MapboxClient from '@mapbox/mapbox-sdk';
+import Geocoding from '@mapbox/mapbox-sdk/services/geocoding';
+import { formatPrice } from '../priceFormatter';
+import { FaMapMarkerAlt } from 'react-icons/fa';
+
+const GetAllLocationCenter = ({ locations }) => {
+  const [viewport, setViewport] = useState({
+    latitude: 10.7769, // Tọa độ latitude của thành phố Hồ Chí Minh
+    longitude: 106.7009, // Tọa độ longitude của thành phố Hồ Chí Minh
+    zoom: 10, //
+  });
+  const [markers, setMarkers] = useState([]);
+
+  const mapboxAccessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
+  mapboxgl.accessToken = mapboxAccessToken;
+
+  useEffect(() => {
+    const mapboxClient = MapboxClient({ accessToken: mapboxAccessToken });
+    const geocodingService = Geocoding(mapboxClient);
+
+    // Xử lý tất cả các địa điểm
+    Promise.all(
+      locations.map((location) => {
+        return geocodingService
+          .forwardGeocode({
+            query: location.addressCenter,
+            limit: 1,
+          })
+          .send()
+          .then((response) => {
+            const match = response.body.features[0];
+            if (match) {
+              const [longitude, latitude] = match.center;
+              return {
+                id: location.id,
+                longitude,
+                latitude,
+                address: match.place_name,
+                name: location.nameCenter,
+                price: location.pricePerHour,
+              };
+            }
+          })
+          .catch((err) => {
+            console.error('Geocoding error:', err);
+          });
+      })
+    ).then((markers) => {
+      setMarkers(markers.filter((marker) => marker !== undefined));
+    });
+  }, [locations, mapboxAccessToken]);
+
+  return (
+    <div style={{ width: '100%', height: '300px' }}>
+      <ReactMapGL
+        {...viewport}
+        width='100%'
+        height='100%'
+        mapboxAccessToken={mapboxAccessToken}
+        onViewportChange={(viewport) => setViewport(viewport)}
+        onMove={(evt) => setViewport(evt.viewState)}
+        mapStyle='mapbox://styles/mapbox/streets-v11'
+        style={{ borderRadius: '0.5rem' }}
+      >
+        <div style={{ position: 'absolute', right: 30, top: 30 }}>
+          <NavigationControl />
+        </div>
+        {markers.map((marker) => (
+          <Marker
+            key={marker.id}
+            longitude={marker.longitude}
+            latitude={marker.latitude}
+          >
+            <div
+              style={{
+                position: 'relative',
+                width: 'max-content',
+                padding: '4px',
+                backgroundColor: 'white',
+                borderRadius: '4px',
+                boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+                zIndex: '1',
+              }}
+            >
+              <FaMapMarkerAlt
+                style={{
+                  color: 'red',
+                  fontSize: '20px',
+                  position: 'absolute',
+                  top: '-24px',
+                  left: '50%',
+                  transform: 'translateX(-50%)',
+                }}
+              />
+              <Link to={`/detail/${marker.id}`}>
+                <div
+                  style={{
+                    fontSize: '12px',
+                    fontWeight: 'bold',
+                    marginBottom: '4px',
+                  }}
+                >
+                  {marker.name}
+                </div>
+              </Link>
+              <div style={{ fontSize: '10px' }}>
+                {formatPrice(marker.price)}đ/giờ
+              </div>
+            </div>
+          </Marker>
+        ))}
+      </ReactMapGL>
+    </div>
+  );
+};
+
+export default GetAllLocationCenter;
