413ec960ef02de707c3f9658b04dfbcfeec34b03
luan0811 <luanntmse173311@fpt.edu.vn>
2024-06-03 11:38:36 +0700

    cấu trúc lại file court manager, đường dẫn

--- frontend/src/pages/login/signupPartner.jsx
+++ frontend/src/pages/login/signupPartner.jsx
@@ -13,7 +13,7 @@ const onFinishFailed = (errorInfo) => {
   console.log("Failed:", errorInfo);
 };
 
-const SignUp = () => (
+const SignupPartner = () => (
   <Row justify="center" align="middle" style={{ minHeight: "100vh" }}>
     <Col
       xs={0}
@@ -31,7 +31,7 @@ const SignUp = () => (
     </Col>
     <Col xs={24} sm={24} md={12} lg={8} xl={6}>
       <div style={{ textAlign: "center", marginBottom: "20px" }}>
-        <h1>Đăng ký trở thành thành viên của Racket Rise</h1>
+        <h1>Đăng ký trở thành cộng tác viên của Racket Rise</h1>
       </div>
       <Form
         name="basic"
@@ -138,12 +138,12 @@ const SignUp = () => (
         </Text>
         <br />
         <Text>
-          Bạn muốn làm cộng tác viên ?{" "}
-          <Link to="/signupPartner">Đăng ký làm cộng tác viên</Link>
+          Bạn chưa có tài khoản thành viên ?{" "}
+          <Link to="/signup">Đăng ký làm thành viên</Link>
         </Text>
       </div>
     </Col>
   </Row>
 );
 
-export default SignUp;
+export default SignupPartner;
